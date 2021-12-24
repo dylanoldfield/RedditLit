679faede09f09a7b24c3988e03785eb05aeb7e3b
dylan <dylan.oldfield@yahoo.com>
2021-12-24 11:38:44 +1100

    added basic rego form

--- web/pages/register.tsx
+++ web/pages/register.tsx
@@ -1,27 +1,71 @@
+import { FormControl, FormLabel, Input, FormHelperText, FormErrorMessage, Button, Box } from '@chakra-ui/react';
 import React from 'react'
 import { useState} from 'react'
 
 interface registerProps {
 
+}
 
+interface field{
+  value: string;
+  error: boolean;
+}
 
+interface formFields {
+    name: field;
+    password:field;
 }
 
 export const Register: React.FC<registerProps> = ({}) => {
-    const [name, setName] = useState<string>('name');
-    const [password, setPassword] = useState<string>('');
+    const [form, setForm] = useState<formFields>({
+      name: {value: '', error: false},
+      password: {value: '', error: false}
+    });
 
     return (
-      <form onSubmit={() => {}}>
-        <label>
-          Name:
-          <input type="text" value ={name} onChange={(e) => {setName(e.target.value)}} />
-        </label>
-        <label>
-          Password:
-          <input type="password" value ={password} onChange={(e) => {setPassword(e.target.value)}} />
-        </label>
-        <input type="submit" value="Submit" />
+      <form onSubmit={(e)=>{
+        e.preventDefault()
+        console.log(form)
+        }}> 
+        <Box maxW={'sm'} mt={'10'} mx={'auto'} alignSelf={'center'}>
+        <FormControl >
+          <FormLabel htmlFor='name'>Name</FormLabel>
+          <Input
+            id='name'
+            type='text'
+            value={form.name.value}
+            onChange={
+              (e) => { setForm({...form, name: {value: e.target.value, error: false}})}
+            }
+          />
+          {!form.name.error ? (
+            <FormHelperText>
+              Please enter full name
+            </FormHelperText>
+          ) : (
+            <FormErrorMessage>Name is required.</FormErrorMessage>
+          )}
+          <FormLabel htmlFor='password' mt={'5'}>Password</FormLabel>
+          <Input
+            id='password'
+            type='password'
+            value={form.password.value}
+            onChange={
+              (e) => { setForm({...form, password: {value: e.target.value, error: false}})}
+            }
+          />
+          {!form.password.error ? (
+            <FormHelperText>
+              Please enter password
+            </FormHelperText>
+          ) : (
+            <FormErrorMessage>Password is required.</FormErrorMessage>
+          )}
+          <Button type='submit' colorScheme={'teal'} size='md'>
+            Submit
+          </Button>
+        </FormControl>
+      </Box>
       </form>
     );
 }
