ba2e70c4ca2d8157300a5dfbca45adfbb29538a8
Dylan Oldfield <dylan.oldfield@yahoo.com>
2022-01-07 16:48:48 +1100

    added login page

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,81 +1,60 @@
 export {}
-// // import { FormControl, FormLabel, Input, FormHelperText, FormErrorMessage, Button, Box } from '@chakra-ui/react';
-// import React from 'react'
-// import { useState} from 'react'
-// import { Wrapper } from '../components/Wrapper';
-// import {useMutation} from 'urql';
-// import {useLoginMutation} from '../generated/graphql'
-// import {FormField} from '../types'
+import React from 'react'
+import {useLoginMutation} from '@/generated/graphql'
+import {Formik, Form, Field} from 'formik'
+import {toErrorMap} from '@/utils/errorMap'
 
-// interface registerProps {
+interface loginProps {
 
-// }
+}
 
-// interface formFields{
-//   name: FormField
-//   password: FormField
-// }
+const FORM_STYLES = {
+  "form": "w-2/4 max-w-screen-sm mx-auto pt-6",
+  "fieldContainer" : "pb-5 flex flex-col",
+  "fieldName" : "text-md pb-1",
+  "fieldInput" : "border border-slate-400 p-1 rounded text-md px-2",
+  "submit" : "bg-teal-500 hover:bg-teal-700 rounded w-20 text-md px-1 py-1 text-white",
+  "fieldError": "text-rose-500 text-sm pt-1"
+}
 
-
-
-// export const Login: React.FC<registerProps> = ({}) => {
-//     const [form, setForm] = useState<formFields>({
-//       name: {value: '', error: {errorMessage: ''}},
-//       password: {value: '', error: {errorMessage: ''}}
-//     });
+export const Login: React.FC<loginProps> = ({}) => {
     
+    const [,login] = useLoginMutation();
 
-//     const [,login] = useLoginMutation();
+    return (
+      <Formik
+        initialValues={{ username: '', password:'' }}
+        onSubmit={async (values, {setErrors}) => {
+          //send registration mutation
+          const response = await login({userDetails: {...values}})
+          // check if error
+          if(response.data?.login.errors){
+            // map to Formik errors
+            setErrors(toErrorMap(response.data.login.errors))
+          } else if (response.data?.login.user){
+            //navigate to landing page
+          }
+          console.log(response)
+          return response;
+        }}
+      >
+        {props => (
+          <Form onSubmit={props.handleSubmit} className={FORM_STYLES.form}>
+            <div className={FORM_STYLES.fieldContainer}>
+              <label htmlFor="username" className={FORM_STYLES.fieldName}> Username </label>
+              <Field className={FORM_STYLES.fieldInput} type="text" name="username" placeholder="username" />
+              {props.errors.username && <div className={FORM_STYLES.fieldError} id="feedback">{props.errors.username}</div>}
+            </div>
+            <div className={FORM_STYLES.fieldContainer}>
+            <label htmlFor='password' className={FORM_STYLES.fieldName}> Password </label>
+            <Field className={FORM_STYLES.fieldInput} type="password" name="password" placeholder="password"/>
+              {props.errors.password && < div className={FORM_STYLES.fieldError} id="feedback">{props.errors.password}</div>}
+            </div>
+            <button className={FORM_STYLES.submit}type="submit"> login </button>
+          </Form>
+        )}
+      </Formik>
+    );
+}
 
-//     return (
-//       <form onSubmit={ async (e)=>{
-//         e.preventDefault()
-//         console.log(form)
-//         const response = await login({userDetails: {username: form.name.value, password: form.password.value}})
-//         console.log(response)
-//         return response;
-//         }}> 
-//         <Wrapper variant="small">
-//         <FormControl >
-//           <FormLabel htmlFor='name'>Name</FormLabel>
-//           <Input
-//             id='name'
-//             type='text'
-//             value={form.name.value}
-//             onChange={
-//               (e) => { setForm({...form, name: {...form.name, value: e.target.value}})}
-//             }
-//           />
-//           {!form.name.error ? (
-//             <FormHelperText>
-//               Please enter full name
-//             </FormHelperText>
-//           ) : (
-//             <FormErrorMessage>{form.name.error?.errorMessage}</FormErrorMessage>
-//           )}
-//           <FormLabel htmlFor='password' mt={'5'}>Password</FormLabel>
-//           <Input
-//             id='password'
-//             type='password'
-//             value={form.password.value}
-//             onChange={
-//               (e) => { setForm({...form, password: {...form.password,value: e.target.value}})}
-//             }
-//           />
-//           {!form.password.error ? (
-//             <FormHelperText>
-//               Please enter password
-//             </FormHelperText>
-//           ) : (
-//             <FormErrorMessage>{form.name.error?.errorMessage}</FormErrorMessage>
-//           )}
-//           <Button type='submit' colorScheme={'teal'} size='md'>
-//             Submit
-//           </Button>
-//         </FormControl>
-//       </Wrapper>
-//       </form>
-//     );
-// }
-
-// export default Login;
+export default Login;
